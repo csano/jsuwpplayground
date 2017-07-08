274f4a9d6133ba6d17f1d2d86bdd51b4caf37338
Chris Sano <cmsano@gmail.com>
2017-07-08 00:15:13 -0700

    Added winjs references

--- JsUwpApp/src/main.ts
+++ JsUwpApp/src/main.ts
@@ -1,17 +1,23 @@
-﻿window.onload = () => {
-  var name = new WinRtComponent.Name("Chris", "Sano");
-  var greeter = new WinRtComponent.Greeter();
-  greeter.ongreet = greet;
-  greeter.sayHelloTo(name);
+﻿
+window.onload = () => {
+    var name = new WinRtComponent.Name("Chris", "Sano");
+    var greeter = new WinRtComponent.Greeter();
+    greeter.ongreet = greet;
+    greeter.sayHelloTo(name);
 
+    var app = WinJS.Application;
+    app.addEventListener("activated", onActivatedHandler, true);
+    app.start();
 }
+
 
 function greet(ev: any) {
   document.getElementById('greeting').innerText = "greeted";
 }
 
 function onActivatedHandler(eventArgs: any) {
+    if (eventArgs.detail.kind == Windows.ApplicationModel.Activation.ActivationKind.protocol) {
 
+    }
+    console.log("activated");
 }
-
-WinJS.Application.addEventListener("activated", onActivatedHandler, false);
